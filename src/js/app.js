1ad355d4e3772d2deea118bbfb282dc3cce625cc
ptrslr <solarptr@gmail.com>
2016-04-20 23:26:06 +0200

    Make shit work in angular

--- src/js/app.js
+++ src/js/app.js
@@ -1,84 +1,84 @@
-(function () {
-   'use strict';
-   // this function is strict...
-    var CLIENT_ID = 'bc2740865fc8d120b6df98beae813823';
+var CLIENT_ID = 'bc2740865fc8d120b6df98beae813823',
+    audioURL = 'http://soundcloud.com/jxnblk/sets/yello',
+    isPlaying = false,
+    scPlayer = new SoundCloudAudio(CLIENT_ID);
 
-    var audioURL = 'http://soundcloud.com/jxnblk/sets/yello';
+function createPlayer() {
+  scPlayer.stop();
+  scPlayer = new SoundCloudAudio(CLIENT_ID);
 
-    var $audioURLInput, $audioURLSubmit;
+  // OR to load PLAYLIST and resolve it's data
+  scPlayer.resolve(audioURL, function (err, playlist) {
+    // do smth with array of `playlist.tracks` or playlist's metadata
+    // e.g. display playlist info in a view etc.
+    // console.log(playlist);
 
-    var $player, $playerPlay, $playerPrevious, $playerNext;
-    var isPlaying = false;
-
-    var scPlayer = new SoundCloudAudio(CLIENT_ID);
-
-    document.addEventListener('DOMContentLoaded', function () {
-        $player = document.getElementById('player');
-        $playerPlay = document.getElementById('player-play');
-        $playerPrevious = document.getElementById('player-previous');
-        $playerNext = document.getElementById('player-next');
-
-        $audioURLInput = document.getElementById("audioURL-input");
-        $audioURLSubmit = document.getElementById("audioURL-submit");
-
-        $audioURLInput.addEventListener('keydown', function() {
-            if (event.keyCode == 13) {
-                audioURL = $audioURLInput.value;
-                createPlayer();
-            }
-        });
-        // submit audio URL
-        $audioURLSubmit.addEventListener('click', function() {
-            // trackURL = $soundcloudURL.value;
-            audioURL = $audioURLInput.value;
-            createPlayer();
-        });
-
-        // play/pause
-        $playerPlay.addEventListener('click', function() {
-            if (isPlaying) {
-                isPlaying = false;
-                scPlayer.pause();
-            } else {
-                isPlaying = true;
-                scPlayer.play();
-            }
-        });
-
-        // next
-        $playerNext.addEventListener('click', function() {
-            scPlayer.next();
-        });
-
-        // previous
-        $playerPrevious.addEventListener('click', function() {
-            scPlayer.previous();
-        });
-
-        createPlayer();
-
-    });
-
-    function createPlayer() {
-        scPlayer.stop();
-        scPlayer = new SoundCloudAudio(CLIENT_ID);
-
-        // OR to load PLAYLIST and resolve it's data
-        scPlayer.resolve(audioURL, function (err, playlist) {
-            // do smth with array of `playlist.tracks` or playlist's metadata
-            // e.g. display playlist info in a view etc.
-            console.log(playlist);
-
-            // once playlist is loaded it can be played
-            if (isPlaying) {
-                scPlayer.play();
-            }
-
-            // for playlists it's possible to switch to another track in queue
-            // e.g. we do it here when playing track is finished
-            scPlayer.on('ended', function () {
-                scPlayer.next();
-            });
-        });
+    // once playlist is loaded it can be played
+    if (isPlaying) {
+      scPlayer.play();
     }
-}());
+
+    // for playlists it's possible to switch to another track in queue
+    // e.g. we do it here when playing track is finished
+    scPlayer.on('ended', function () {
+      scPlayer.next();
+    });
+  });
+}
+
+createPlayer();
+
+var myApp = angular.module('silentParty', []);
+
+// http://stackoverflow.com/questions/17470790/how-to-use-a-keypress-event-in-angularjs
+myApp.directive('myEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.myEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
+
+myApp.controller('AudioSubmitController', function() {
+  var self = this;
+
+  self.submit = function() {
+    audioURL = self.audioURL;
+    createPlayer();
+  };
+  self.keydown = function() {
+
+  };
+});
+
+myApp.controller('PlayerController', function() {
+  var self = this;
+
+  self.createPlayer = function() {
+
+  };
+  self.playPause = function() {
+    if (isPlaying) {
+      isPlaying = false;
+      scPlayer.pause();
+      self.playPauseLabel = "Play";
+    } else {
+      isPlaying = true;
+      scPlayer.play();
+      self.playPauseLabel = "Pause";
+    }
+
+  };
+  self.previous = function() {
+    scPlayer.previous();
+  };
+  self.next = function() {
+    scPlayer.next();
+  };
+});
