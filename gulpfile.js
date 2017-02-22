f1911609b5fa5a10da3cd6a67827f199b4087961
ptrslr <solarptr@gmail.com>
2017-02-22 22:13:04 +0100

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -43,16 +43,6 @@ gulp.task('img', function() {
         .pipe(gulp.dest('src/img'))
 });
 
-// gulp.task('js', function() {
-//     return gulp.src('src/**/*.js')
-//         .pipe(sourcemaps.init())
-//         .pipe(babel())
-//         .pipe(concat('all.js'))
-//         // .pipe(webpack())
-//         .pipe(sourcemaps.write('.'))
-//         .pipe(gulp.dest('dist'));
-// });
-
 var webpackConf = {
     output: {
         filename: 'bundle.js',
